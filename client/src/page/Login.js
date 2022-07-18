21116e791c49de8ab0ff2b0ba37fedffc04c710a
Andi Usman Balo <47916303+andibalo@users.noreply.github.com>
2022-07-18 15:29:14 +0700

    add login input validations and login footer

--- client/src/page/Login.js
+++ client/src/page/Login.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react"
-import { Inputs, Header } from "../components/login";
+import React, { useEffect, useState } from "react"
+import { Inputs, Header, Footer } from "../components/login";
 import { AuthBox } from "../components/shared"
+import { validateLoginForm } from "../utils/validators";
 
 export const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isFormValid, setIsFormValid] = useState(false);
 
+    useEffect(() => {
+      setIsFormValid(validateLoginForm({ email, password }));
+    }, [email, password, setIsFormValid]);
+  
+    const handleLogin = () => {
+    //   const userDetails = {
+    //     mail,
+    //     password,
+    //   };
+  
+    //   login(userDetails, history);
+    };
+  
     return (
         <AuthBox>
             <Header />
@@ -16,6 +31,7 @@ export const Login = () => {
                 password={password}
                 setPassword={setPassword}
             />
+            <Footer isFormValid={isFormValid} handleLogin={handleLogin} />
         </AuthBox>
     )
 }
